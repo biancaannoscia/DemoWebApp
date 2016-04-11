0a710abe7c93ec732f38b81426ccac45d5b0a3ee
Bianca Annoscia <bianca94@bu.edu>
2016-04-11 14:35:40 -0400

    fixed bugs

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
 
-function myFunction() {
+/*function myFunction() {
   document.getElementById("demo").innerHTML = "NEWWW PARAGRAPH #javascript #swag";
-}
+} */
+
+console.log("Let's make a database!");
 
