115f1345a38766dfdabb071e7bd1d3bf41a81c58
Bianca Annoscia <bianca94@bu.edu>
2016-04-11 16:49:24 -0400

    remove body-parser

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var bodyParser = require('body-parser')
+
 var mongoose = require('mongoose');
 
 var app = express();
@@ -12,7 +12,7 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.use(bodyParser());
+
 
 mongoose.connect(process.env.MONGOLAB_URI);
 
